3b3536a19e5fd4d71ab540acda29cba95310f3e9
user <user@DESKTOP-PFCSEEP>
2019-02-06 23:07:09 -0500

    ini

---
+++ src/main/java/com/example/demo/JpaAndHibernateTutorialForBeginnersApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateTutorialForBeginnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateTutorialForBeginnersApplication.class, args);
+	}
+
+}
+
